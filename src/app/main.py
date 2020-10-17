52fe9868db71cab0cfddb298673b4ddf9c90cee6
Ernesto Revilla <ernesto.revilla@gmail.com>
2020-10-17 14:54:04 +0200

    feat(basic-chat): small functional demo.

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,30 @@
+import socketio
+
 from fastapi import FastAPI
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+sio = socketio.AsyncServer(async_mode='asgi')
+app.mount('/sio', socketio.ASGIApp(sio))
+
+
+@sio.on('connect')
+def sio_connect(sid, environ):
+    print('A user connected')
+
+
+@sio.on('disconnect')
+def sio_disconnect(sid):
+    print('User disconnected')
+
+
+@sio.on('chat message')
+async def chat_message(sid, msg):
+    print('Message: %s' % msg)
+    await sio.emit('chat message', msg)
 
 
 @app.get("/")
@@ -11,3 +35,8 @@ async def home() -> dict:
         'message': "Hello world",
         'version': "0.1",
     }
+
+
+@app.get("/chat", response_class=FileResponse)
+def chat() -> HTMLResponse:
+    return FileResponse('chat.html')
