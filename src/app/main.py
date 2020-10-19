8bf89d78e9a271168f6c57396de29ce56f8bc038
Ernesto Revilla <ernesto.revilla@gmail.com>
2020-10-19 22:09:40 +0200

    chore(chat): docstrings, correct fileresponse.

--- src/app/main.py
+++ src/app/main.py
@@ -1,28 +1,34 @@
+"""The FastAPI main module"""
+import os
 import socketio
 
 from fastapi import FastAPI
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+path = os.path.dirname(__file__)
+app.mount("/static", StaticFiles(directory=os.path.join(path, "static")), name="static")
 
 sio = socketio.AsyncServer(async_mode='asgi')
-app.mount('/sio', socketio.ASGIApp(sio))
+app.mount('/sio', socketio.ASGIApp(sio))  # socketio adds automatically /socket.io/ to the URL.
 
 
 @sio.on('connect')
 def sio_connect(sid, environ):
+    """A user / browser tab has connected"""
     print('A user connected')
 
 
 @sio.on('disconnect')
 def sio_disconnect(sid):
+    """A user / browser tab has disconnected"""
     print('User disconnected')
 
 
 @sio.on('chat message')
 async def chat_message(sid, msg):
+    """A chat message has arrived"""
     print('Message: %s' % msg)
     await sio.emit('chat message', msg)
 
@@ -38,5 +44,5 @@ async def home() -> dict:
 
 
 @app.get("/chat", response_class=FileResponse)
-def chat() -> HTMLResponse:
+def chat() -> FileResponse:
     return FileResponse('chat.html')
