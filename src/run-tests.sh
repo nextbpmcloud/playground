514fef0d4a626f77d1036d7640e646d909e27e2f
Ernesto Revilla <ernesto.revilla@gmail.com>
2020-10-23 13:55:57 +0200

    feat(docker): Move files

--- src/run-tests.sh
+++ src/run-tests.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 RUNNER=pipenv
+DIR=../test-results
+
 if [ -n "$1" ]
 then
     RUNNER=$1
 fi
 
-DIR=test-results
 mkdir -p $DIR
 
 function msgrun() {
@@ -18,21 +19,21 @@ function msgsuccess(){
 
 # run first syntax and code style checks
 msgrun flake8
-$RUNNER run flake8 --tee --output-file $DIR/flake8.txt
+$RUNNER run flake8 --tee --output-file $DIR/flake8.txt app
 msgsuccess $? flake8
 $RUNNER run flake8_junit $DIR/flake8.txt $DIR/flake8_junit.xml >/dev/null
 
 if [ "$RES" = "ok" ]; then
   # run type checks
   msgrun mypy
-  $RUNNER run mypy --junit-xml $DIR/mypy_junit.xml src
+  $RUNNER run mypy --junit-xml $DIR/mypy_junit.xml app
   msgsuccess $? mypy
 fi
 
 if [ "$RES" = "ok" ]; then
   # run unit tests with coverage checking
   msgrun pytest
-  $RUNNER run pytest --cov src --junitxml=$DIR/pytest_junit.xml
+  $RUNNER run pytest --cov app --junitxml=$DIR/pytest_junit.xml
   msgsuccess $? pytest
 fi
 
@@ -40,4 +41,10 @@ echo "Tests done."
 if [ "$RES" != "ok" ]; then
     exit 1
 fi
-bash <(curl -s https://codecov.io/bash)
+pipenv run coverage html
+pipenv run coverage xml
+echo "Downloading coverage uploader"
+curl -s https://codecov.io/bash > upload_codecov
+chmod +x upload_codecov
+./upload_codecov -f coverage.xml
+mv htmlcov coverage.xml $DIR
